5c46b0ed010d71f589021320437ad04a7054b86c
AhmadShopnil <ahmadshopnil75@gmail.com>
2023-04-22 22:11:44 +0600

    fix slide

--- components/ProductDetailsSlider/ProductDetailsSlider.jsx
+++ components/ProductDetailsSlider/ProductDetailsSlider.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Carousel } from "react-responsive-carousel";
+import Image from "next/image";
 
 const ProductDetailsSlider = () => {
   return (
@@ -12,13 +13,19 @@ const ProductDetailsSlider = () => {
         thumbWidth={60}
         className="productCarousel"
       >
-        <img src="/assets/img/p1.png" alt="" />
-        <img src="/assets/img/p2.png" alt="" />
-        <img src="/assets/img/p3.png" alt="" />
-        <img src="/assets/img/p4.png" alt="" />
-        <img src="/assets/img/p5.png" alt="" />
-        <img src="/assets/img/p6.png" alt="" />
-        <img src="/assets/img/p7.png" alt="" />
+        <Image
+          className="image"
+          src="/assets/img/p1.png"
+          alt=""
+          width={400}
+          height={500}
+        />
+        <Image src="/assets/img/p2.png" alt="" width={400} height={500} />
+        <Image src="/assets/img/p3.png" alt="" width={400} height={500} />
+        <Image src="/assets/img/p4.png" alt="" width={400} height={500} />
+        <Image src="/assets/img/p5.png" alt="" width={400} height={500} />
+        <Image src="/assets/img/p6.png" alt="" width={400} height={500} />
+        <Image src="/assets/img/p7.png" alt="" width={400} height={500} />
       </Carousel>
     </div>
   );
